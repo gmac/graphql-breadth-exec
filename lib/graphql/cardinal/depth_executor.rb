acb900fd0337a1dd1ff3c8f0b14b5608f7901995
Greg MacWilliam <greg.macwilliam@shopify.com>
2025-06-13 10:27:40 -0400

    depth-first almost works.

--- lib/graphql/cardinal/depth_executor.rb
+++ lib/graphql/cardinal/depth_executor.rb
@@ -10,7 +10,6 @@ module GraphQL
         @resolvers = resolvers
         @document = document
         @root_object = root_object
-        @data = {}
         @exec_count = 0
       end
 
@@ -18,67 +17,68 @@ module GraphQL
         @query = GraphQL::Query.new(@schema, document: @document) # << for schema reference
         operation = @query.selected_operation
         parent_type = @query.root_type_for_operation(operation.operation_type)
-        exec_object_scope(parent_type, operation.selections, @root_object, @data, path: [])
-        @data
+        exec_object_scope(parent_type, operation.selections, @root_object, path: [])
       end
 
       private
 
-      def exec_object_scope(parent_type, selections, source, response, path:)
+      def exec_object_scope(parent_type, selections, source, path:)
+        response = {}
         selections.each do |node|
           case node
           when GraphQL::Language::Nodes::Field
-            field = @query.get_field(parent_type, node.name)
-            field_type = field.type.unwrap
-            field_key = node.alias || node.name
-            path.push(field_key)
+            node_type = @query.get_field(parent_type, node.name).type
+            named_type = node_type.unwrap
+            field_name = node.alias || node.name
+            path.push(field_name)
 
             resolved_source = @resolvers.dig(parent_type.graphql_name, node.name).call(source)
             @exec_count += 1
 
-            puts "#{parent_type.graphql_name}.#{node.name}"
-            puts resolved_source
-
-            if field_type.kind.leaf?
-              response[field_key] = resolved_source
-            elsif field_type.list?
-              next_response = Array.new({}, resolved_source.size)
-              exec_list_scope(parent_type, node.selections, resolved_source, next_response, path: path)
+            response[field_name] = if node_type.list?
+              node_type = node_type.of_type while node_type.non_null?
+              exec_list_scope(node_type, node.selections, resolved_source, path: path)
+            elsif named_type.kind.leaf?
+              resolved_source
             else
-              exec_object_scope(field_type, node.selections, resolved_source, {}, path: path)
+              exec_object_scope(named_type, node.selections, resolved_source, path: path)
             end
             path.pop
 
           when GraphQL::Language::Nodes::InlineFragment
             fragment_type = node.type ? @query.get_type(node.type.name) : parent_type
-            exec_object_scope(fragment_type, node.selections, source, response, path: path)
+            exec_object_scope(fragment_type, node.selections, source, path: path)
 
           when GraphQL::Language::Nodes::FragmentSpread
             fragment = @query.fragments[node.name]
             fragment_type = @query.get_type(fragment.type.name)
-            exec_object_scope(fragment_type, node.selections, source, response, path: path)
+            exec_object_scope(fragment_type, node.selections, source, path: path)
 
           else
             raise DocumentError.new("selection node type")
           end
         end
+
+        response
       end
 
-      def exec_list_scope(parent_type, selections, source, response, path:)
-        list_type = list_type.of_type while list_type.non_null?
-        next_type = list_type.of_type
+      def exec_list_scope(parent_type, selections, sources, path:)
+        parent_type = parent_type.of_type while parent_type.non_null?
+        next_node_type = parent_type.of_type
+        named_type = parent_type.unwrap
 
-        list_response = []
-        source.each do |src|
-          if next_type.list?
-            list_response << build_list_response(next_type, src, next_sources, next_responses)
+        sources.map.with_index do |src, i|
+          path.push(i)
+          result = if next_node_type.list?
+            resolve_list_scope(next_node_type, selections, src, path: path)
+          elsif named_type.kind.leaf?
+            src
           else
-            next_sources << src
-            next_responses << {}
-            list_response << next_responses.last
+            exec_object_scope(named_type, selections, src, path: path)
           end
+          path.pop
+          result
         end
-        list_response
       end
     end
   end
